a1cf02ae758777270b7ebdc7c2bd03734d970062
Arturo Guevara <R2rog@MacBook-Pro-de-Arturo.local>
2019-02-21 18:39:30 -0600

    Initial commit

---
+++ app/src/main/java/com/a01272373/lab4/MainActivity.java
@@ -0,0 +1,37 @@
+package com.a01272373.lab4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class MainActivity extends AppCompatActivity {
+
+    float x1,x2,y1,y2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        switch(touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+                if(x1<x2){
+                    Intent i = new Intent(MainActivity.this,activity_swipe_left.class);
+                    startActivity(i);
+                }else if(x1>x2){
+                    Intent i = new Intent(MainActivity.this, activity_swipe_right.class);
+                    startActivity(i);
+                }
+                break;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/a01272373/lab4/activity_swipe_right.java
@@ -0,0 +1,13 @@
+ package com.a01272373.lab4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_swipe_right extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_right);
+    }
+}
